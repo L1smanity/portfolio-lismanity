449155704af934cef004a7130b00c7e1309c44b7
L1smanity <lismanity@gmail.com>
2024-11-21 14:21:13 +0100

    changes

--- src/pages/SingleProject.tsx
+++ src/pages/SingleProject.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import projectCategories from "../assets/work";
 import Navbar from "../components/Navbar";
